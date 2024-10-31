e163cca61e0162eab205cda96d690105d261c08b
Tark Wight <arsentiy.gonda@gmail.com>
2024-10-31 20:46:36 +0700

    [Setup][TB.Feed]  view controller

--- MovieCatalogProj/MovieCatalog/MovieCatalog/Presentation/Modules/TabBar/Feed/FeedViewController.swift
+++ MovieCatalogProj/MovieCatalog/MovieCatalog/Presentation/Modules/TabBar/Feed/FeedViewController.swift
@@ -8,16 +8,82 @@
 import UIKit
 
 final class FeedViewController: UIViewController {
-    private let viewModel: FeedViewModel
+    private let viewModel = FeedViewModel()
     
-    public init(viewModel: FeedViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-        self.view.backgroundColor = .systemBackground
-        self.title = NSLocalizedString("feed", comment: "")
+    private let loadingView = UIActivityIndicatorView(style: .large)
+    private let movieCardView = MovieCardView()
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Logo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupBindings()
+        viewModel.fetchRandomMovie()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(logoImageView)
+        view.addSubview(loadingView)
+        view.addSubview(movieCardView)
+        
+        loadingView.center = view.center
+        loadingView.startAnimating()
+        
+        movieCardView.isHidden = true
+        movieCardView.addGestureRecognizer(UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:))))
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        movieCardView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 78),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 60.72),
+            logoImageView.heightAnchor.constraint(equalToConstant: 32),
+            
+          movieCardView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 24),
+//            movieCardView.topAnchor.constraint(equalTo: view.topAnchor, constant: 134),
+            movieCardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            movieCardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            movieCardView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32)
+        ])
+    }
+    
+    private func setupBindings() {
+        viewModel.onLoading = { [weak self] isLoading in
+            self?.loadingView.isHidden = !isLoading
+            self?.movieCardView.isHidden = isLoading
+        }
+        
+        viewModel.onMoviesFetched = { [weak self] movie in
+            self?.movieCardView.configure(with: movie)
+        }
+        
+        viewModel.onError = { error in
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+        guard let movie = movieCardView.movie, let movieId = movie.id else {
+            print("Movie ID is nil.")
+            return
+        }
+        
+        if gesture.direction == .right {
+            viewModel.addFavoriteMovie(movieId)
+        } else if gesture.direction == .left {
+            viewModel.hideMovie(movieId)
+        }
     }
 }
