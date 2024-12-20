15eace5dd267c75537f1ceeb72fde347dfb4dfef
TarkWight <arsentiy.gonda@gmail.com>
2024-12-20 15:05:19 +0700

    Update keychain manager
    -- Added protocol
    -- Added error handling

--- MovieCatalogProj/ClientAPI/ClientAPI/Source/KeychainManager/KeychainManager.swift
+++ MovieCatalogProj/ClientAPI/ClientAPI/Source/KeychainManager/KeychainManager.swift
@@ -8,48 +8,126 @@
 import Foundation
 import Security
 
-public class KeychainManager {
-    
-    public static let shared = KeychainManager()
-    private let tokenKey = "BearerTokenKey"
+import Foundation
+import Security
 
-    public func saveToken(_ token: String) -> Bool {
-        guard let tokenData = token.data(using: .utf8) else { return false }
-        
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: tokenKey,
-            kSecValueData as String: tokenData
-        ]
-        
-        SecItemDelete(query as CFDictionary)
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        return status == errSecSuccess
+protocol KeychainServiceProtocol {
+    func saveToken(_ token: String) throws
+    func retrieveToken() throws -> String
+    func updateToken(_ newToken: String) throws
+    func deleteToken() throws
+}
+
+final class KeychainService: KeychainServiceProtocol {
+
+    enum KeychainError: LocalizedError {
+        case invalidData
+        case itemNotFound
+        case duplicateItem
+        case incorrectAttributeForClass
+        case unexpectedStatus(OSStatus)
+
+        var errorDescription: String? {
+            switch self {
+            case .invalidData:
+                return "Invalid data"
+            case .itemNotFound:
+                return "Item not found"
+            case .duplicateItem:
+                return "Duplicate Item"
+            case .incorrectAttributeForClass:
+                return "Incorrect Attribute for Class"
+            case .unexpectedStatus(let status):
+                return "Unexpected error - \(status)"
+            }
+        }
     }
-    
-    public func getToken() -> String? {
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: tokenKey,
-            kSecReturnData as String: true,
-            kSecMatchLimit as String: kSecMatchLimitOne
-        ]
-        
-        var dataTypeRef: AnyObject?
-        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
-        
-        guard status == errSecSuccess, let data = dataTypeRef as? Data else { return nil }
-        return String(data: data, encoding: .utf8)
+
+    private enum Key: String {
+        case accessToken
     }
-    
-    public func deleteToken() -> Bool {
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: tokenKey
-        ]
-        
-        let status = SecItemDelete(query as CFDictionary)
-        return status == errSecSuccess
+
+    func saveToken(_ token: String) throws {
+        guard let data = token.data(using: .utf8) else {
+            throw KeychainError.invalidData
+        }
+
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecValueData: data,
+            kSecAttrAccount: Key.accessToken.rawValue
+        ] as CFDictionary
+
+        let status = SecItemAdd(query, nil)
+
+        guard status == errSecSuccess else {
+            throw convertError(status)
+        }
+    }
+
+    func retrieveToken() throws -> String {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: Key.accessToken.rawValue,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecReturnData: kCFBooleanTrue as Any
+        ] as CFDictionary
+
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query, &result)
+
+        guard status == errSecSuccess else {
+            throw convertError(status)
+        }
+
+        guard let data = result as? Data else {
+            throw KeychainError.invalidData
+        }
+
+        return String(decoding: data, as: UTF8.self)
+    }
+
+    func updateToken(_ newToken: String) throws {
+        guard let data = newToken.data(using: .utf8) else {
+            throw KeychainError.invalidData
+        }
+
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: Key.accessToken.rawValue
+        ] as CFDictionary
+        let attributesToUpdate = [kSecValueData: data] as CFDictionary
+
+        let status = SecItemUpdate(query, attributesToUpdate)
+
+        guard status == errSecSuccess else {
+            throw convertError(status)
+        }
+    }
+
+    func deleteToken() throws {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: Key.accessToken.rawValue
+        ] as CFDictionary
+
+        let status = SecItemDelete(query)
+
+        guard status == errSecSuccess else {
+            throw convertError(status)
+        }
+    }
+
+    private func convertError(_ status: OSStatus) -> KeychainError {
+        switch status {
+        case errSecItemNotFound:
+            return .itemNotFound
+        case errSecDataTooLarge:
+            return .invalidData
+        case errSecDuplicateItem:
+            return .duplicateItem
+        default:
+            return .unexpectedStatus(status)
+        }
     }
 }
