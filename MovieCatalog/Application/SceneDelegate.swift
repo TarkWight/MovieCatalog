f2c402e82b642bef9414ded7525bffe077286603
TarkWight <arsentiy.gonda@gmail.com>
2025-01-05 15:33:05 +0700

    Added app icon and setup launch screen
    -- Deleted Content view controller

--- MovieCatalog/Application/SceneDelegate.swift
+++ MovieCatalog/Application/SceneDelegate.swift
@@ -12,18 +12,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
         let window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        let navigation = UINavigationController(rootViewController: viewController)
-        window.rootViewController = navigation
-        
-        self.window = window
-        window.makeKeyAndVisible()
 
+//        let fetchProfileUseCase = FetchProfileUseCase()
+//        let fetchFavoriteMoviesUseCase = FetchFavoriteMoviesUseCase()
+
+        let appCoordinator = AppCoordinator(
+            window: window
+//            fetchProfileUseCase: fetchProfileUseCase,
+//            fetchFavoriteMoviesUseCase: fetchFavoriteMoviesUseCase
+        )
+        appCoordinator.start()
+
+        self.window = window
     }
 
 }
