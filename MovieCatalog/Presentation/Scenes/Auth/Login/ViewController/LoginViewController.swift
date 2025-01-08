f8c950165b499e9be6a46eb54b37f03c4f63db2a
TarkWight <arsentiy.gonda@gmail.com>
2025-01-09 02:02:37 +0700

    Added login view controller

---
+++ MovieCatalog/Presentation/Scenes/Auth/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,138 @@
+//  LoginViewController.swift
+//  MovieCatalog
+//
+//  Created by Tark Wight on 06.01.2025.
+//
+
+import UIKit
+
+final class LoginViewController: BaseViewController {
+
+    // MARK: - Properties
+    private let viewModel: LoginViewModel
+
+    private let usernameField = CustomTextField(placeholder: LocalizedKey.Auth.LigIn.TextField.username, type: .text)
+    private let passwordField = CustomTextField(placeholder: LocalizedKey.Auth.LigIn.TextField.password, type: .password)
+    private let signInButton = CustomButton()
+
+    // MARK: - Initializers
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bindViewModel()
+    }
+
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = UIColor(named: Constants.Colors.background)
+
+        // Настройка полей и кнопки
+        view.addSubview(usernameField)
+        usernameField.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(passwordField)
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(signInButton)
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.setTitle("Войти", for: .normal) // Устанавливаем текст кнопки
+        signInButton.configure(for: .disabled) // Кнопка изначально неактивна
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            usernameField.heightAnchor.constraint(equalToConstant: Constants.Layout.inputFieldHeight),
+            usernameField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.Layout.sidePadding),
+            usernameField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.Layout.sidePadding),
+            usernameField.bottomAnchor.constraint(equalTo: passwordField.topAnchor, constant: -Constants.Layout.inputStackSpacing),
+
+            passwordField.heightAnchor.constraint(equalToConstant: Constants.Layout.inputFieldHeight),
+            passwordField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.Layout.sidePadding),
+            passwordField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.Layout.sidePadding),
+            passwordField.bottomAnchor.constraint(equalTo: signInButton.topAnchor, constant: -Constants.Layout.buttonTopOffset),
+
+            signInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.Layout.sidePadding),
+            signInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.Layout.sidePadding),
+            signInButton.heightAnchor.constraint(equalToConstant: Constants.Layout.inputFieldHeight),
+            signInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constants.Layout.safeAreaPadding)
+        ])
+    }
+
+    // MARK: - ViewModel Binding
+    private func bindViewModel() {
+        usernameField.onTextChanged = { [weak self] text in
+            self?.viewModel.handle(.usernameChanged(text))
+            self?.updateSignInButtonState()
+        }
+
+        passwordField.onTextChanged = { [weak self] text in
+            self?.viewModel.handle(.passwordChanged(text))
+            self?.updateSignInButtonState()
+        }
+
+        viewModel.delegate = self
+    }
+
+    private func updateSignInButtonState() {
+        if isFormValid {
+            signInButton.configure(for: .default) // Активная кнопка
+        } else {
+            signInButton.configure(for: .disabled) // Неактивная кнопка
+        }
+    }
+
+    private var isFormValid: Bool {
+        return !(usernameField.textField.text?.isEmpty ?? true) &&
+               !(passwordField.textField.text?.isEmpty ?? true)
+    }
+
+    // MARK: - Actions
+    @objc private func didTapSignIn() {
+        viewModel.handle(.logInTapped)
+    }
+}
+
+// MARK: - LoginViewModelDelegate
+extension LoginViewController: LoginViewModelDelegate {
+    func didUpdateLoadingState(isLoading: Bool) {
+        signInButton.isUserInteractionEnabled = !isLoading
+    }
+
+    func didEncounterError(_ message: String) {
+        let alert = UIAlertController(title: NSLocalizedString("error", comment: ""), message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("ok", comment: ""), style: .default))
+        present(alert, animated: true)
+    }
+
+    func didCompleteLogin() {
+        print("Login Successful")
+    }
+}
+
+// MARK: - Constants
+private enum Constants {
+    enum Layout {
+        static let sidePadding: CGFloat = 24
+        static let inputStackSpacing: CGFloat = 8
+        static let buttonTopOffset: CGFloat = 32
+        static let inputFieldHeight: CGFloat = 48
+        static let safeAreaPadding: CGFloat = 24
+    }
+
+    enum Colors {
+        static let background = "AppDark"
+    }
+}
