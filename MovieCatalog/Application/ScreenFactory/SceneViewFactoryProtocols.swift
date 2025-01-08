1b7a2639bdd64b8f3aea21509e6f817bccacf099
TarkWight <arsentiy.gonda@gmail.com>
2025-01-09 01:52:28 +0700

    Added scene view factory protocol

---
+++ MovieCatalog/Application/ScreenFactory/SceneViewFactoryProtocols.swift
@@ -0,0 +1,12 @@
+//
+//  SceneViewFactoryProtocols.swift
+//  MovieCatalog
+//
+//  Created by Tark Wight on 07.01.2025.
+//
+
+import Foundation
+
+protocol AuthCoordinatorFactory: LoginViewFactory,
+                                 WelcomeViewFactory
+{}
