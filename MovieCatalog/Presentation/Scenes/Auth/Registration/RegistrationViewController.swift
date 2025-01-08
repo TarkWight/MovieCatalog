b0169a87d7c52023b72de691169166252d359f22
TarkWight <arsentiy.gonda@gmail.com>
2025-01-09 01:59:48 +0700

    Added registration view controller

---
+++ MovieCatalog/Presentation/Scenes/Auth/Registration/RegistrationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationViewController.swift
+//  MovieCatalog
+//
+//  Created by Tark Wight on 04.01.2025.
+//
+
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    
+}
