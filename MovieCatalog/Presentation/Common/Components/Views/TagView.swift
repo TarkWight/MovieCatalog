445c58f976153d43ed0dcbf2c51fee0a23322051
TarkWight <arsentiy.gonda@gmail.com>
2025-01-12 23:23:29 +0700

    Added tag view

---
+++ MovieCatalog/Presentation/Common/Components/Views/TagView.swift
@@ -0,0 +1,74 @@
+//
+//  TagView.swift
+//  MovieCatalog
+//
+//  Created by Tark Wight on 12.01.2025.
+//
+
+
+import UIKit
+
+final class TagView: UIView {
+    private let label = UILabel()
+    
+    var text: String? {
+        didSet {
+            label.text = text
+            updateWidth()
+        }
+    }
+    
+    var isActive: Bool = false {
+        didSet {
+            backgroundColor = isActive ? UIColor(named: "AccentColor") : UIColor(named: "AppGray")
+        }
+    }
+    
+    init(text: String, isActive: Bool = false) {
+        super.init(frame: .zero)
+        self.text = text
+        self.isActive = isActive
+        setupView()
+        addTapGesture()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+        addTapGesture()
+    }
+    
+    func setupView() {
+        backgroundColor = isActive ? UIColor(named: "AccentColor") : UIColor(named: "AppGray")
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+        heightAnchor.constraint(equalToConstant: 28).isActive = true
+        
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = UIColor(named: "AppWhite")
+        label.text = text
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func updateWidth() {
+        let labelWidth = label.intrinsicContentSize.width
+        widthAnchor.constraint(equalToConstant: labelWidth + 4).isActive = true
+    }
+    
+    private func addTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(toggleActiveState))
+        addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func toggleActiveState() {
+        isActive.toggle()
+    }
+}
